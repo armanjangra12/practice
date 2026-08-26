c14dee485123cea0ab901565c6d76dbb85c2e120
armanjangra12 <armanjangra12@gmail.com>
2026-08-26 18:30:18 +0530

    yo.py

---
+++ yo.py
@@ -0,0 +1,9 @@
+# Take inputs from the user
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Convert inputs from text strings to numbers and add them
+result = float(num1) + float(num2)
+
+# Display the final answer
+print("The total sum is:", result)
